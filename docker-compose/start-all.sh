02a551a2381e6f47bca6c3795b3aa86a20a128e2
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2024-02-24 09:17:03 -0600

    Parametrize which Docker Compose file will be executed: https://github.com/adempiere/adempiere-ui-gateway/issues/45

--- docker-compose/start-all.sh
+++ docker-compose/start-all.sh
@@ -41,5 +41,36 @@ else
     echo "Directory \"$PERSISTENTDIR\" exists already: no need to create it"
 fi
 
+# Find out which Docker Compose file will be used.
+# The script must be called with the flag "-d" + one of the following parameters [auth, cache, develop, storage, vue, default]
+# e.g.: ./start-all.sh -d vue
+# If script is called without or with a wrong flag, 'default' will be taken.
+# e.g.: ./start-all.sh
+while getopts d: flag
+do
+    case "${flag}" in
+        d) docker_compose_option=${OPTARG};;
+        *) docker_compose_option=default;;
+    esac
+done
+echo "Script called with parameter: \"$docker_compose_option\"";
+    
+case "${docker_compose_option}" in
+    auth)    docker_compose_file=docker-compose-auth.yml;;
+    cache)   docker_compose_file=docker-compose-cache.yml;;
+    develop) docker_compose_file=docker-compose-develop.yml;;
+    storage) docker_compose_file=docker-compose-storage.yml;;
+    vue)     docker_compose_file=docker-compose-vue.yml;;
+    default) docker_compose_file=docker-compose.yml;;
+    *)       docker_compose_file=docker-compose.yml;;
+esac
+
+# Save Docker Compose file name in file .DOCKER_COMPOSE_FILE
+# This will be used in scripts stop-all.sh and stop-and-delete.all.sh to stop the started services.
+savefile=.DOCKER_COMPOSE_FILE
+echo $docker_compose_file > $savefile
+
+
+echo "Docker Compose will be executed with file: \"$docker_compose_file\"";
 cp env_template .env
-docker compose up -d
+docker compose -f $docker_compose_file up -d
