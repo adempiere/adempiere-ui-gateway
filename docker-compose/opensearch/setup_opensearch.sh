82bc9026d68d64da984197333d0748310cd50e2a
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2024-03-12 14:07:49 -0600

    Update .conf files in preparation for merge with main branch

--- docker-compose/opensearch/setup_opensearch.sh
+++ docker-compose/opensearch/setup_opensearch.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 
-echo "Setup for allows increase size..."
-curl --location --request PUT 'http://opensearch-node:9200/_all/_settings' --header 'Content-Type: application/json' --data '{"index.blocks.read_only_allow_delete": null}'
+echo "Setup for allows increase size... ${OPENSEARCH_HOST}"
+curl --location --request PUT "$OPENSEARCH_HOST}/_all/_settings" \
+	--header 'Content-Type: application/json' \
+	--data '{"index.blocks.read_only_allow_delete": null}'
+
+
 echo "Creating repository..."
-curl --location --request PUT 'http://opensearch-node:9200/_snapshot/default-repository' --header 'Content-Type: application/json' --data '{"type": "fs","settings": {"location": "/mnt/snapshots"}}'
+curl --location --request PUT "${OPENSEARCH_HOST}/_snapshot/default-repository" \
+	--header 'Content-Type: application/json' \
+	--data '{"type": "fs","settings": {"location": "/mnt/snapshots"}}'
+
+
 echo "Restoring snapshot..."
-curl --location --request POST 'http://opensearch-node:9200/_snapshot/default-repository/1/_restore' --header 'Content-Type: application/json' --data ''
+curl --location --request POST "${OPENSEARCH_HOST}/_snapshot/default-repository/1/_restore" \
+	--header 'Content-Type: application/json' \
+	--data ''
