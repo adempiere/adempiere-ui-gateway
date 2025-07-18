5957e0198e2c65454ede235945bbe2772668ec50
felixgacriac <felixgaciac@gmail.com>
2025-07-17 21:02:47 -0400

    fix stop-all profile name.

--- docker-compose/stop-all.sh
+++ docker-compose/stop-all.sh
@@ -3,7 +3,7 @@
 # Script to stop all services started with the Docker Compose file.
 # The services were called with this Docker Compose file via script "start-all.sh".
 
-DOCKER_COMPOSE_FILE=docker-compose-profle.yml
+DOCKER_COMPOSE_FILE=docker-compose.yml
 
 echo "All services started with the Docker Compose file \"$DOCKER_COMPOSE_FILE\" will be stopped!"
 docker compose -f $DOCKER_COMPOSE_FILE down
